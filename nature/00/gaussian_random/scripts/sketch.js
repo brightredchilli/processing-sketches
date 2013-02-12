3be317266403389c7734f9162530ee57671d25b0
Ying Quan Tan <ying.quan.tan@gmail.com>
2013-02-12 18:28:06 -0500

    Excercise1.4 - Random Paint Splattering with Gaussian Distribution

--- nature/00/gaussian_random/scripts/sketch.js
+++ nature/00/gaussian_random/scripts/sketch.js
@@ -11,30 +11,44 @@ define(["scripts/lib/processing-1.4.1-api.js", "scripts/lib/gaussian_random.js"]
     //declare classes
 
 
-    function Walker () {
+    function Walker (red, green, blue) {
       var width = 15, height = 15;
       var x = processing.width/2;
       var y = processing.height/2;
-      var sd = 60, mean = processing.width/2;
+      var sd = 60, colorSd=30, meanX = x, meanY = y;
+
+      var r = red, g = green , b = blue;
 
       this.display = function () {
         processing.noStroke();
-        processing.fill(255, 10);
         processing.ellipse(x,y, width, height);
       }
 
       this.step = function () {
-        var num = Z.nextGaussian();
-        x = sd * num + mean
+        var numX = Z.nextGaussian();
+        var numY = Z.nextGaussian();
+        x = Z.nextGaussianValue(sd, meanX);
+        y = Z.nextGaussianValue(sd, meanY);
 
+
+        var randR = Z.nextGaussianValue(colorSd, r);
+        var randG = Z.nextGaussianValue(colorSd, g);
+        var randB = Z.nextGaussianValue(colorSd, b);
+
+        processing.fill(randR, randG, randB);
       }
     }
 
     //declare variables
     processing.size(800,600);
     processing.background(0);
-    var walker = new Walker();
+    var walker = new Walker(230, 50, 50);
     var Z = new Ziggurat();
+
+    Ziggurat.prototype.nextGaussianValue = function(sd, mean) {
+      var num = this.nextGaussian();
+      return num * sd + mean;
+    }
     
     //functions
 
