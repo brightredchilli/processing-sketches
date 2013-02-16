37269f954d00e07dc802e9a994ee3ae199352bf1
Ying Quan Tan <ying.quan.tan@gmail.com>
2013-02-16 17:52:16 -0500

    tried out some stuff

--- nature/00/noise2d/scripts/sketch.js
+++ nature/00/noise2d/scripts/sketch.js
@@ -13,29 +13,29 @@ define(["scripts/lib/processing-1.4.1-api.js"], function() {
     processing.size(500,500);
     processing.background(0);
 
-    var xoff = 0; 
     var xoffSpan = document.getElementById("xoff_display");
     var yoffSpan = document.getElementById("yoff_display");
-    var skip = 0;
+
+    var xoff = 0, yoff = 2; 
     function procDraw() {
+
+      var currentXOff = 0, currentYOff = 0;
       //functions
-      skip++;
-      if ((skip % 2) == 0) {
-        return;
-      }
       processing.loadPixels();
       for (var x = 0; x < processing.width; x++) {
-        var yoff = 0;
         for (var y = 0; y < processing.height; y++) {
-          var brightness = processing.map(processing.noise(xoff,yoff), 0, 1, 0, 255);
+          var brightness = processing.map(processing.noise(xoff + currentXOff ,yoff+ currentYOff), 0, 1, 0, 255);
           processing.pixels.setPixel(x + y*processing.width, processing.color(brightness));
-          yoff += 0.003;
+          currentYOff += 0.003;
         }
-        xoff += 0.003;
+        currentXOff += 0.003;
       }
       processing.updatePixels();
       xoffSpan.textContent = xoff;
       yoffSpan.textContent = yoff;
+
+      xoff += 0.003;
+      yoff += 0.003;
 
     };
 
