8b447f61896728611f27523bfcf93d579196b69c
Ying Quan Tan <ying.quan.tan@gmail.com>
2013-02-17 22:09:09 -0500

    experimented with 3rd dimension perlin noise, did not work

--- nature/00/noise2d/scripts/sketch.js
+++ nature/00/noise2d/scripts/sketch.js
@@ -4,7 +4,8 @@ define(["scripts/lib/processing-1.4.1-api.js"], function() {
 
   //processing setup
   module.start = function(canvas) {
-    processing = new Processing(canvas, start); }
+    processing = new Processing(canvas, start); 
+  };
 
   function start(processing) {
     //declare classes
@@ -16,7 +17,7 @@ define(["scripts/lib/processing-1.4.1-api.js"], function() {
     var xoffSpan = document.getElementById("xoff_display");
     var yoffSpan = document.getElementById("yoff_display");
 
-    var xoff = 0, yoff = 2; 
+    var zoff = 0;
     function procDraw() {
 
       var currentXOff = 0;
@@ -25,20 +26,17 @@ define(["scripts/lib/processing-1.4.1-api.js"], function() {
       for (var x = 0; x < processing.width; x++) {
         var currentYOff = 0;
         for (var y = 0; y < processing.height; y++) {
-          var brightness = processing.map(processing.noise(xoff + currentXOff ,yoff+ currentYOff), 0, 1, 0, 255);
+          var brightness = processing.map(processing.noise(currentXOff, currentYOff, zoff), 0, 1, 0, 255);
           processing.pixels.setPixel(x + y*processing.width, processing.color(brightness));
           currentYOff += 0.003;
         }
         currentXOff += 0.003;
       }
       processing.updatePixels();
-      xoffSpan.textContent = xoff;
-      yoffSpan.textContent = yoff;
 
-      xoff += 0.009;
-      yoff += 0.009;
+      zoff += 0.005;
 
-    };
+    }
 
     processing.draw = procDraw;
   }
