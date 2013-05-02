216151ae4533787a417e0c49b1fc1ccae863e3d5
Ying Quan Tan <ying.quan.tan@gmail.com>
2013-05-01 23:04:14 -0400

    added revolute joint

---
+++ nature/05/revolute_joint/scripts/main.js
@@ -0,0 +1,29 @@
+require.config({
+  paths: {
+    app: "./",
+    processing: "lib/processing-1.4.1-api", //p for processing
+    domready: "lib/domready",
+    gaussian: "lib/gaussian_random",
+    mover: "lib/mover",
+    particle: "lib/particle",
+    particlesystem: "lib/particlesystem",
+    box2dweb: "lib/box2dweb",
+    b2box: "lib/b2box",
+    b2boundary: "lib/b2boundary",
+    b2surface: "lib/b2surface",
+    pbox2d: "lib/pbox2d"
+  }
+});
+
+define(["sketch"], function(Sketch) {
+  var canvas = document.getElementById("processing-canvas");
+  Sketch.initialize(canvas);
+});
+
+
+ 
+
+
+
+
+
