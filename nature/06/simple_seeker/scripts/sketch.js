0003d594ab6bcdc137c10357a24853655cb84648
Ying Quan Tan <ying.quan.tan@gmail.com>
2013-05-14 19:51:06 -0400

    fixed sketch, added some pursuit

--- nature/06/simple_seeker/scripts/sketch.js
+++ nature/06/simple_seeker/scripts/sketch.js
@@ -1,16 +1,17 @@
-define(["processing", "toxi", "two"], function(Processing, toxi, Two) {
+define(["processing", "toxi", "two"], function(Processing, toxijs, Two) {
 
   var module = {};
 
-  var Vec2D = toxi.geom.Vec2D;
-  var Rect = toxi.geom.Rect;
-  var GravityBehavior = toxi.physics2d.behaviors.GravityBehavior;
-  var VerletPhysics2D = toxi.physics2d.VerletPhysics2D;
-  var VerletParticle2D = toxi.physics2d.VerletParticle2D;
-  var VerletSpring2D = toxi.physics2d.VerletSpring2D;
-  var VerletConstrainedSpring2D = toxi.physics2d.VerletConstrainedSpring2D;
-  var VerletMinDistanceSpring2D = toxi.physics2d.VerletMinDistanceSpring2D;
-  var PerlinNoise = toxi.math.noise.PerlinNoise;
+  var Vec2D = toxijs.geom.Vec2D;
+  var Rect = toxijs.geom.Rect;
+  var GravityBehavior = toxijs.physics2d.behaviors.GravityBehavior;
+  var VerletPhysics2D = toxijs.physics2d.VerletPhysics2D;
+  var VerletParticle2D = toxijs.physics2d.VerletParticle2D;
+  var VerletSpring2D = toxijs.physics2d.VerletSpring2D;
+  var VerletConstrainedSpring2D = toxijs.physics2d.VerletConstrainedSpring2D;
+  var VerletMinDistanceSpring2D = toxijs.physics2d.VerletMinDistanceSpring2D;
+  var PerlinNoise = toxijs.math.noise.PerlinNoise;
+  var RectConstraint = toxijs.physics2d.constraints.RectConstraint;
 
   //processing setup
   module.initialize = function() {
@@ -31,7 +32,7 @@ define(["processing", "toxi", "two"], function(Processing, toxi, Two) {
     var two = new Two({width : 800, height : 600}).appendTo(contentElement);
     var svg = two.renderer.domElement;
     var physics = new VerletPhysics2D();
-    physics.setWorldBounds = new Rect(0, 0, two.width, two.height);
+    physics.setWorldBounds(new Rect(0, 0, two.width, two.height));
 
     var background = two.makeRectangle(two.width/2, two.height/2, two.width, two.height);
     background.stroke = "#aaa";
@@ -56,32 +57,78 @@ define(["processing", "toxi", "two"], function(Processing, toxi, Two) {
     };
 
     Vehicle.prototype.steer = function (x, y) {
+      this.clearForce();
+      var maxspeed = 5;
       var target = new Vec2D(x, y);
       var desired = target.sub(this);
-      var steer = desired.sub(this.getVelocity());
-      steer.limit(0.3); //maxspeed
-      if(desired.magnitude() > 50) {
-        this.addForce(steer);
+      var d = desired.magnitude();
+
+      desired.normalize();
+
+      if (d < 100) {
+        desired.scaleSelf(Math.floor((d/100) * maxspeed));
+      } else {
+        desired.scaleSelf(maxspeed);
       }
-      
+      var steer = desired.sub(this.getVelocity());
+      steer.scaleSelf(0.3); //maxspeed
+      if (d > 10) {
+        this.addForce(steer);
+      } else {
+        console.log("UNDER !!  ");
+        this.clearVelocity();
+      }
     };
 
+    Vehicle.prototype.getRandomX = function () {
+      if (this.__perlinStepX === undefined) {
+        this.__perlinStepX = Math.random() * 100;
+      }
+      this.__perlinStepX += 0.5;
+      return this.noise.noise(this.__perlinStepX) - 0.5;
+    };
+
+    Vehicle.prototype.getRandomY = function () {
+      if (this.__perlinStepY === undefined) {
+        this.__perlinStepY = Math.random() * 100;
+      }
+      this.__perlinStepY += 0.5;
+      return this.noise.noise(this.__perlinStepY) - 0.5;
+    };
+
+    Vehicle.prototype.randomForce = function () {
+      if (!this.noise) {
+        this.noise = new PerlinNoise();
+      }
+      var f = new Vec2D(this.getRandomX(), this.getRandomY());
+      f.scaleSelf(1);
+      this.addForce(f);
+    };
 
     var v = new Vehicle(300, 300);
     var t = new Vehicle(500, 500);
+    t.shape.fill = "blue";
+    t.shape.stroke = "green";
+    t.randomSteer = function () {
+      this.randomForce();
+      
+    };
 
-    var target; 
+    //constrain t to not move out of screen
+    /*
+    var center = new Vehicle(two.width/2, two.height/2);
+    center.lock();
+    physics.addParticle(center);
+    */
+    //physics.addSpring(new VerletSpring2D(t, center, 200, 0.03, 1000));
+
 
     function mouseDragged (e) {
       var pt = cursorPoint(e);
-      if (target) {
-        target.translation.x = pt.x;
-        target.translation.y = pt.y;
-      } else {
-        target = two.makeCircle(pt.x, pt.y, 5);
-        target.fill = "blue";
-      }
-
+      t.lock();
+      t.x = pt.x;
+      t.y = pt.y;
+      t.unlock();
     };
 
     svg.addEventListener("mousedown", mouseDragged, false);
@@ -89,9 +136,9 @@ define(["processing", "toxi", "two"], function(Processing, toxi, Two) {
     two.bind('update', function(frameCount) {
       physics.update();
       v.display();
-      if (target) {
-        v.steer(target.translation.x, target.translation.y);
-      }
+      t.display();
+      t.randomSteer();
+      v.steer(t.x, t.y);
     }).play();
   }
   return module;
